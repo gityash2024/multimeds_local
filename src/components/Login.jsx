9acdf0c57373fb1b21f020776ffafd75cc9811ac
yash jangid <yash@ith.tech>
2024-03-21 14:09:24 +0530

    opt code fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,14 +4,14 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import OtpInput from "react-otp-input";
 import { gql, useMutation } from "@apollo/client";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import LoginCarousel from "./LoginCarousel";
 import PrimaryButton from "./PrimaryButton";
 import Google from "../assets/login/googleLogo.svg";
 import Edit from "../assets/login/editIcon.svg";
 import Check from "../assets/login/checkIcon.svg";
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Loader from "./loader";
 const SENDOTP = gql`
   mutation sendOTP($input: OTPInput!) {
@@ -38,22 +38,67 @@ const VERIFYOTP = gql`
       message
       token
       isNewUser
-      userDetails {
+      user {
         id
+        fullName
         contactNumber
+        email
+        profilePicture
+        walletBalance
+        role
+        successfulReferrals
+        referralDiscountPercentage
+        remainingReferralDiscounts
+        currentAddress {
+          id
+          houseNumber
+          aptOrBuildingName
+          streetOrAreaName
+          city
+          pincode
+          state
+          label
+        }
+        department {
+          id
+          name
+          description
+          permissions
+        }
+        addresses {
+          id
+          houseNumber
+          aptOrBuildingName
+          streetOrAreaName
+          city
+          pincode
+          state
+          label
+        }
+        prescriptions {
+          id
+          url
+          userId
+          isApproved
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
       }
     }
   }
 `;
 
+
 const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
   const loginRef = useRef();
-  const navigate=useNavigate();
-  const [errorMessage, setError] = useState('');
+  const navigate = useNavigate();
+  const [errorMessage, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [isOtpWindow, setIsOtpWindow] = useState(false);
-  const [loginMethod, setLoginMethod] = useState('phone number');
+  const [loginMethod, setLoginMethod] = useState("phone number");
   const [loginId, setLoginId] = useState("");
   const [otp, setOtp] = useState("");
 
@@ -77,9 +122,9 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
   const openOtpWindow = async (e) => {
     e.preventDefault();
     // setInputValue('');
-    setError('');
+    setError("");
 
-    toast.success('Otp Sent Succesfully');
+    toast.success("Otp Sent Succesfully");
     setLoading(true);
 
     try {
@@ -92,16 +137,19 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
         mutation = sendEmailOTP;
       }
 
-      const input = loginMethod !== "email" ? { countryCode: '+91', phoneNumber: inputValue } : { email: inputValue };
+      const input =
+        loginMethod !== "email"
+          ? { countryCode: "+91", phoneNumber: inputValue }
+          : { email: inputValue };
 
       const { data } = await mutation({ variables: { input } });
 
       setLoading(false);
 
-      if (data[method].status === "SUCCESS") { 
+      if (data[method].status === "SUCCESS") {
         setIsOtpWindow(true);
       } else {
-        toast.error(`Error: ${data[method].message}`);              
+        toast.error(`Error: ${data[method].message}`);
       }
     } catch (error) {
       console.error("Error in sending OTP:", error);
@@ -115,27 +163,30 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
   };
 
   const handleSubmit = async () => {
-    setError('');
+    setError("");
     toast.info(`Verifying OTP`);
     const variObj = {
-      countryCode: '+91',
+      countryCode: "+91",
       phoneNumber: inputValue,
       otp: otp,
     };
-  
+
     // Set loading to true here to show the loader during verification
     setLoading(true);
-  
+
     try {
       const res = await verifyOTP({ variables: { input: variObj } });
-      
+
       // Process the response
-      console.log('verify otp res is', res);
+      console.log("verify otp res is", res);
       if (res.data.verifyOTP.status === "SUCCESS") {
-        localStorage.setItem('token', res.data.verifyOTP.token);
+        localStorage.setItem("token", res.data.verifyOTP.token);
         setUserDetails(res.data.verifyOTP.token);
-        localStorage.setItem('userInfo', JSON.stringify(res.data.verifyOTP.userDetails));
-        navigate('/refferal-login');
+        localStorage.setItem(
+          "userInfo",
+          JSON.stringify(res.data.verifyOTP.userDetails)
+        );
+        navigate("/refferal-login");
       } else {
         toast.error(`Otp not verified!`);
       }
@@ -146,32 +197,31 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
       // Ensure loading is set to false after the operation is complete
       setLoading(false);
     }
-  
+
     setIsLogin(false);
   };
-  
 
   const handleClick = () => {
     // setInputValue('');
-    setError('');
+    setError("");
     setLoginMethod(loginMethod === "phone number" ? "email" : "phone number");
   };
 
   const validateInput = (value) => {
-    if (loginMethod === 'email') {
+    if (loginMethod === "email") {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       if (!emailRegex.test(value)) {
-        setError('Please enter a valid email address');
+        setError("Please enter a valid email address");
         return false;
       }
     } else {
       const numberRegex = /^\d{10}$/;
       if (!numberRegex.test(value)) {
-        setError('Please enter a valid 10-digit number');
+        setError("Please enter a valid 10-digit number");
         return false;
       }
     }
-    setError('');
+    setError("");
     return true;
   };
 
@@ -179,12 +229,15 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
     const value = e.target.value;
     setInputValue(value);
     validateInput(value);
-    setLoginId(loginMethod === "email" ? `${value.slice(0, 4)}xxxxxx.${value.slice(-4)}` : `XXXXX${value.slice(-4)}x`);
+    setLoginId(
+      loginMethod === "email"
+        ? `${value.slice(0, 4)}xxxxxx.${value.slice(-4)}`
+        : `XXXXX${value.slice(-4)}x`
+    );
   };
 
   return (
     <div>
-
       <div
         ref={ref}
         className="w-screen h-screen fixed top-0 left-0 z-50 flex justify-center items-center bg-black bg-opacity-40"
@@ -210,18 +263,32 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
                   <input
                     value={inputValue}
                     onChange={handleChange}
-                    placeholder={loginMethod === 'email' ? 'Enter email' : 'Enter mobile number'}
+                    placeholder={
+                      loginMethod === "email"
+                        ? "Enter email"
+                        : "Enter mobile number"
+                    }
                     className="min-h-10 rounded border border-[#CBD5E1] focus:outline-none text-[0.875rem] py-[0.813rem] px-4 placeholder:text-[#E2E8F0] font-medium placeholder:text-[0.875rem] placeholder:font-medium"
                   />
-                  {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-                  <PrimaryButton disable={(errorMessage || !inputValue)?true:false}  handleClick={openOtpWindow} title="LOGIN" />
+                  {errorMessage && (
+                    <p className="text-red-500">{errorMessage}</p>
+                  )}
+                  <PrimaryButton
+                    disable={errorMessage || !inputValue ? true : false}
+                    handleClick={openOtpWindow}
+                    title="LOGIN"
+                  />
                   <p className="text-[0.75rem] text-[#475569]">
                     By logging in, you have agreed to our{" "}
                     <Link to="/legal" className="text-[#FBA79B]" href="">
                       terms and conditions
                     </Link>{" "}
                     and{" "}
-                    <Link to="/privacypolicy" className="text-[#FBA79B]" href="">
+                    <Link
+                      to="/privacypolicy"
+                      className="text-[#FBA79B]"
+                      href=""
+                    >
                       privacy policy
                     </Link>
                   </p>
@@ -240,7 +307,9 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
                     className="w-full h-full  rounded p-4 gap-2 border border-[#CBD5E1]"
                   >
                     <h1 className=" uppercase h-full flex justify-center items-center min-h-[1.563rem] font-HelveticaNeueMedium text-[#031B89]">
-                      {loginMethod === "phone number" ? "email" : "phone number"}
+                      {loginMethod === "phone number"
+                        ? "email"
+                        : "phone number"}
                     </h1>
                   </button>
                 </div>
@@ -278,28 +347,29 @@ const Login = ({ ref, isLogin, setIsLogin, setUserDetails }) => {
                     containerStyle={"flex gap-2"}
                     inputStyle={`min-w-[2.25rem] h-[2.5rem] py-[0.813rem] px-3 ${
                       otp === "" ? "border border-[#CBD5E1]" : null
-                    } ${
-                      otp !== ""
-                        ? "border border-[#CBD5E1]"
-                        : null
-                    } rounded`}
-                
+                    } ${otp !== "" ? "border border-[#CBD5E1]" : null} rounded`}
                   />
-              
                 </div>
-              
               </div>
               <div className="flex flex-col gap-2">
                 <p className="text-[#0F172A] text-[0.75rem]">
                   Didn’t get an OTP?{" "}
-                  <button onClick={openOtpWindow}
+                  <button
+                    onClick={openOtpWindow}
                     className="text-[#7487FF]  font-HelveticaNeueMedium"
                   >
                     Resend Code
                   </button>
                 </p>
-                <Link >
-                  <PrimaryButton  disable={( !otp )?true:false}  handleClick={()=>{setLoading(true);handleSubmit()}} title="SUBMIT" />
+                <Link>
+                  <PrimaryButton
+                    disable={!otp ? true : false}
+                    handleClick={() => {
+                      setLoading(true);
+                      handleSubmit();
+                    }}
+                    title="SUBMIT"
+                  />
                 </Link>
               </div>
               <p className="text-[#0F172A]  text-[0.75rem] font-HelveticaNeueMedium">
